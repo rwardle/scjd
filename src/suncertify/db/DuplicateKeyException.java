8205ba8af471989cf74be111e7598bcc6d668a96
rwardle <none@none>
2005-05-30 18:40:37 +0100

    [svn] Initial import.
    --HG--
    branch : trunk

---
+++ src/suncertify/db/DuplicateKeyException.java
@@ -0,0 +1,52 @@
+package suncertify.db;
+
+/**
+ * TODO
+ * 
+ * @author Richard Wardle
+ */
+public class DuplicateKeyException extends Exception {
+
+    /**
+     * Creates a new <code>DuplicateKeyException</code>.
+     */
+    public DuplicateKeyException() {
+        super();
+    }
+
+    /**
+     * Creates a new <code>DuplicateKeyException</code> with the specified
+     * detail message.
+     * 
+     * @param message
+     *            The detail message.
+     */
+    public DuplicateKeyException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a new <code>DuplicateKeyException</code> with the specified
+     * cause.
+     * 
+     * @param cause
+     *            The cause.
+     */
+    public DuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Creates a new <code>DuplicateKeyException</code> with the specified
+     * message and cause.
+     * 
+     * @param message
+     *            The message.
+     * @param cause
+     *            The cause.
+     */
+    public DuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ src/suncertify/db/RecordNotFoundException.java
@@ -0,0 +1,53 @@
+package suncertify.db;
+
+/**
+ * Exception thrown if a record does not exist or has been deleted in the
+ * database.
+ * 
+ * @author Richard Wardle
+ */
+public class RecordNotFoundException extends Exception {
+
+    /**
+     * Creates a new <code>RecordNotFoundException</code>.
+     */
+    public RecordNotFoundException() {
+        super();
+    }
+
+    /**
+     * Creates a new <code>RecordNotFoundException</code> with the specified
+     * detail message.
+     * 
+     * @param message
+     *            The detail message.
+     */
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a new <code>RecordNotFoundException</code> with the specified
+     * cause.
+     * 
+     * @param cause
+     *            The cause.
+     */
+    public RecordNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Creates a new <code>RecordNotFoundException</code> with the specified
+     * message and cause.
+     * 
+     * @param message
+     *            The message.
+     * @param cause
+     *            The cause.
+     */
+    public RecordNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
