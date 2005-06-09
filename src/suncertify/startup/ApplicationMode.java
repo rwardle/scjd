f139f6c4ac8aff67b2f9f7061813d660bb15e762
rwardle <none@none>
2005-06-09 17:21:17 +0100

    [svn] Added javadoc.
    --HG--
    branch : trunk

--- src/suncertify/startup/ApplicationMode.java
+++ src/suncertify/startup/ApplicationMode.java
@@ -1,7 +1,7 @@
 /*
  * ApplicationMode.java
  *
- * Created on 08 June 2005, 10:26
+ * Created on 08 June 2005
  */
 
 
@@ -9,25 +9,39 @@ package suncertify.startup;
 
 
 /**
- * 
+ * Represents the run mode of the application.
  *
  * @author Richard Wardle
  */
-public class ApplicationMode {
-    
+public final class ApplicationMode {
+
+    /**
+     * Client mode: the GUI runs and connects to the database server over the
+     * network.
+     */
     public static final ApplicationMode CLIENT = new ApplicationMode("client");
-    
+
+    /**
+     * Server mode: the server exposes database functionality to clients over
+     * the network.
+     */
     public static final ApplicationMode SERVER = new ApplicationMode("server");
-    
-    public static final ApplicationMode STANDALONE 
-            = new ApplicationMode("standalone");
-    
+
+    /**
+     * Standalone mode: the GUI runs and connects to the database locally.
+     */
+    public static final ApplicationMode STANDALONE =
+            new ApplicationMode("standalone");
+
     private final String name;
-    
+
     private ApplicationMode(final String name) {
         this.name = name;
     }
-    
+
+    /**
+     * {@inheritDoc}
+     */
     public String toString() {
         return this.name;
     }
