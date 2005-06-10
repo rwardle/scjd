e2610edcb48f7ac16c941cee340c632178ea30d7
rwardle <none@none>
2005-06-10 11:46:07 +0100

    [svn] Checkstyle changes.
    --HG--
    branch : trunk

--- src/suncertify/startup/ConfigurationViewFactory.java
+++ src/suncertify/startup/ConfigurationViewFactory.java
@@ -9,20 +9,26 @@ package suncertify.startup;
 
 
 /**
+ * A simple parameterised factory for creating instances of
+ * {@link ConfigurationView} based on the application mode.
  *
  * @author Richard Wardle
  */
-public class ConfigurationViewFactory {
-    
-    /**
-     * Creates a new instance of ConfigurationViewFactory 
-     */
+public final class ConfigurationViewFactory {
+
     private ConfigurationViewFactory() {
     }
-    
-    public static ConfigurationView createConfigurationDialog(ApplicationMode mode) {
+
+    /**
+     * Creates a new ConfigurationView based on the application mode.
+     *
+     * @param mode The application mode.
+     * @return The configuration view.
+     */
+    public static ConfigurationView createConfigurationView(
+            ApplicationMode mode) {
         ConfigurationView view = null;
-        
+
         if (mode == ApplicationMode.CLIENT) {
             view = new ClientConfigurationDialog();
         } else if (mode == ApplicationMode.SERVER) {
@@ -30,10 +36,9 @@ public class ConfigurationViewFactory {
         } else if (mode == ApplicationMode.STANDALONE) {
             view = new StandaloneConfigurationDialog();
         } else {
-            // TODO check this
-            assert false;
+            assert false : mode;
         }
-        
+
         return view;
     }
 }
