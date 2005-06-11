e7a8618fbe697fa985799cae9517f3dabad2d2f1
rwardle <none@none>
2005-06-11 12:07:59 +0100

    [svn] Checkstyle changes.
    --HG--
    branch : trunk

--- src/suncertify/startup/ConfigurationPresenter.java
+++ src/suncertify/startup/ConfigurationPresenter.java
@@ -12,48 +12,54 @@ import java.awt.event.ActionListener;
 
 
 /**
+ * Encapsulates the presentation behaviour of the configuration dialog.
+ *
  * @author Richard Wardle
  */
-public class ConfigurationPresenter {
+public final class ConfigurationPresenter {
 
+    /** Indicates that the Cancel button was clicked in the view. */
     public static final int RETURN_CANCEL = 0;
+
+    /** Indicates that the OK button was clicked in the view. */
     public static final int RETURN_OK = 1;
 
     private Configuration configuration;
-    private int returnStatus;
     private ConfigurationView view;
+    private int returnStatus;
 
     /**
-     * 
+     * Creates a new instance of ConfigurationPresenter.
+     *
+     * @param configuration The configuration model.
+     * @param view The configuration view.
+     * @throws NullPointerException If the configuration or view is null.
      */
     public ConfigurationPresenter(Configuration configuration,
             ConfigurationView view) {
+        if (configuration == null || view == null) {
+            throw new NullPointerException(
+                    "configuration and view arguments must be non-null");
+        }
+
         this.configuration = configuration;
         this.view = view;
-        this.returnStatus = RETURN_CANCEL;
-        addOKListener();
-        addCancelListener();
+        this.returnStatus = ConfigurationPresenter.RETURN_CANCEL;
+        addOKButtonListener();
+        addCancelButtonListener();
         loadViewFromModel();
     }
 
     /**
      * Gets the returnStatus.
-     * 
-     * @return Returns the returnStatus.
+     *
+     * @return The returnStatus.
      */
     public int getReturnStatus() {
         return this.returnStatus;
     }
 
-    private void addCancelListener() {
-        this.view.addCancelButtonListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                cancelButtonActionPerformed();
-            }
-        });
-    }
-
-    private void addOKListener() {
+    private void addOKButtonListener() {
         this.view.addOkButtonListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 okButtonActionPerformed();
@@ -61,34 +67,33 @@ public class ConfigurationPresenter {
         });
     }
 
-    private void cancelButtonActionPerformed() {
-        this.view.close();
-    }
-
-    private void okButtonActionPerformed() {
-        this.returnStatus = RETURN_OK;
+    void okButtonActionPerformed() {
+        this.returnStatus = ConfigurationPresenter.RETURN_OK;
         saveViewToModel();
         this.view.close();
     }
 
+    private void addCancelButtonListener() {
+        this.view.addCancelButtonListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cancelButtonActionPerformed();
+            }
+        });
+    }
+
+    void cancelButtonActionPerformed() {
+        this.view.close();
+    }
+
     private void loadViewFromModel() {
+        this.view.setDatabaseFilePath(this.configuration.getDatabaseFilePath());
         this.view.setServerAddress(this.configuration.getServerAddress());
         this.view.setServerPort(this.configuration.getServerPort());
-        this.view.setDatabaseFilePath(this.configuration.getDatabaseFilePath());
     }
-    
+
     private void saveViewToModel() {
+        this.configuration.setDatabaseFilePath(this.view.getDatabaseFilePath());
         this.configuration.setServerAddress(this.view.getServerAddress());
         this.configuration.setServerPort(this.view.getServerPort());
-        this.configuration.setDatabaseFilePath(this.view.getDatabaseFilePath());
     }
-    
-    // TODO
-    // addWindowListener(new WindowAdapter() {
-    //
-    // public void windowClosing(WindowEvent event) {
-    // closeDialog();
-    // }
-    // });
-
 }
